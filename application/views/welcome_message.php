63e4792c547c77160bc57ab0242cdfeee88ce2a4
David Organ <david.sorgan@gmail.com>
2015-06-25 20:29:33 +0100

    CSs changes

--- application/views/welcome_message.php
+++ application/views/welcome_message.php
@@ -5,7 +5,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 <head>
 	<meta charset="utf-8">
 	<title>Welcome to CodeIgniter</title>
-
+	<link rel="stylesheet" type="text/css" href="../css/style.css">
 	<style type="text/css">
 
 	::selection { background-color: #E13300; color: white; }
